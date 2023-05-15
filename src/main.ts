03c5b169bdf351999a99c6c93204eb486d8f073e
devmrwor <michaelryanrm18@gmail.com>
2023-05-15 19:58:23 +0700

    add windicss

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import router from './router'
 import App from '@/App.vue'
 
+import 'virtual:windi.css'
+
 const app = createApp(App)
 
 app.use(router)
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import AutoImport from 'unplugin-auto-import/vite'
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    WindiCSS(),
     AutoImport({
       include: [
         /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
